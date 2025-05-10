0b5c49d8d65c4a40d69aaddd5ec8844cb207cc24
juan sebastian navarrete <juan.sebastian.navarrete@correounivalle.edu.co>
2025-05-10 16:07:13 -0500

    Implementación del menú principal para seleccionar juegos

---
+++ juegos/main.cpp
@@ -0,0 +1,11 @@
+#include "Ahorcado.hpp"
+#include "GestorArchivo.hpp"
+#include "Juego.hpp"
+#include "MenuPrincipal.hpp"
+#include "Parejas.hpp"
+
+int main() {
+    // Crea un objeto de la clase MenuPrincipal
+    
+    return 0;
+}
