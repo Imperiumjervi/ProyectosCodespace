4c44a0655a84b34742b2f21de09fee9a31dd555c
Imperiumjervi <rodriguez.jefferson@correounivalle.edu.co>
2025-05-12 18:36:56 -0500

    Menus hechos

--- juegos/main.cpp
+++ juegos/main.cpp
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-#include "Ahorcado.hpp"
-#include "GestorArchivo.hpp"
-#include "Juego.hpp"
 #include "MenuPrincipal.hpp"
-#include "Parejas.hpp"
 
 int main() {
-    // Crea un objeto de la clase MenuPrincipal
-    MenuPrincipal menu;
-    menu.mostrarMenu();
-    return 0;
-}
-=======
-#include "MenuPrincipal.hpp"
-#include <iostream>
-
-int main() {
-  MenuPrincipal menucito;
-
-  menucito.mostrarMenu();
-
+  // Crea un objeto de la clase MenuPrincipal
+  MenuPrincipal menu;
+  menu.mostrarMenu();
   return 0;
 }
->>>>>>> CambiosJeffer
