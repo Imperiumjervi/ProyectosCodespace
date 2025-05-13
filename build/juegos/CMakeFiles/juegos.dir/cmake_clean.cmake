0785ee305fca39a1280fc7160c2320529a152a18
Imperiumjervi <163766795+Imperiumjervi@users.noreply.github.com>
2025-05-13 13:00:10 +0000

    Revert ""Neta no queria hacer esto""
    This reverts commit 91cb6e94a6caa5788aae8b955a23cb42a71a2f8e.

--- build/juegos/CMakeFiles/juegos.dir/cmake_clean.cmake
+++ build/juegos/CMakeFiles/juegos.dir/cmake_clean.cmake
@@ -1,12 +1,19 @@
 file(REMOVE_RECURSE
   "../bin/juegos"
   "../bin/juegos.pdb"
+  "CMakeFiles/juegos.dir/link.d"
   "CMakeFiles/juegos.dir/main.cpp.o"
+  "CMakeFiles/juegos.dir/main.cpp.o.d"
   "CMakeFiles/juegos.dir/src/Ahorcado.cpp.o"
+  "CMakeFiles/juegos.dir/src/Ahorcado.cpp.o.d"
   "CMakeFiles/juegos.dir/src/GestorArchivo.cpp.o"
+  "CMakeFiles/juegos.dir/src/GestorArchivo.cpp.o.d"
   "CMakeFiles/juegos.dir/src/Juego.cpp.o"
+  "CMakeFiles/juegos.dir/src/Juego.cpp.o.d"
   "CMakeFiles/juegos.dir/src/MenuPrincipal.cpp.o"
+  "CMakeFiles/juegos.dir/src/MenuPrincipal.cpp.o.d"
   "CMakeFiles/juegos.dir/src/Parejas.cpp.o"
+  "CMakeFiles/juegos.dir/src/Parejas.cpp.o.d"
 )
 
 # Per-language clean rules from dependency scanning.
