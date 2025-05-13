9ef5846a0c2da1aeb40f459235b1e173196b7e99
Juan Sebastian Navarrete <163350568+PROGJUAN317@users.noreply.github.com>
2025-05-13 13:59:55 +0000

    .

--- taller/include/clasepordefecto.hpp
+++ taller/include/clasepordefecto.hpp
@@ -1,7 +1,11 @@
 #pragma once
 #include <iostream>
 
-class ClasePorDefecto {
+class Cuenta {
+private:
+  double saldo;
+  std::string numero;
 public:
-  void saludar();
+  Cuenta();
+  Cuenta(double saldo, std::string numero);   
 };
