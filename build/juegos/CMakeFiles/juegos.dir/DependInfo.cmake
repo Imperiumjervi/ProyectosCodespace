91cb6e94a6caa5788aae8b955a23cb42a71a2f8e
Imperiumjervi <163766795+Imperiumjervi@users.noreply.github.com>
2025-05-13 12:51:53 +0000

    "Neta no queria hacer esto"

--- build/juegos/CMakeFiles/juegos.dir/DependInfo.cmake
+++ build/juegos/CMakeFiles/juegos.dir/DependInfo.cmake
@@ -1,28 +1,25 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/codespace/ProyectosCodespace/ProyectosCodespace/juegos/main.cpp" "/home/codespace/ProyectosCodespace/ProyectosCodespace/build/juegos/CMakeFiles/juegos.dir/main.cpp.o"
+  "/home/codespace/ProyectosCodespace/ProyectosCodespace/juegos/src/Ahorcado.cpp" "/home/codespace/ProyectosCodespace/ProyectosCodespace/build/juegos/CMakeFiles/juegos.dir/src/Ahorcado.cpp.o"
+  "/home/codespace/ProyectosCodespace/ProyectosCodespace/juegos/src/GestorArchivo.cpp" "/home/codespace/ProyectosCodespace/ProyectosCodespace/build/juegos/CMakeFiles/juegos.dir/src/GestorArchivo.cpp.o"
+  "/home/codespace/ProyectosCodespace/ProyectosCodespace/juegos/src/Juego.cpp" "/home/codespace/ProyectosCodespace/ProyectosCodespace/build/juegos/CMakeFiles/juegos.dir/src/Juego.cpp.o"
+  "/home/codespace/ProyectosCodespace/ProyectosCodespace/juegos/src/MenuPrincipal.cpp" "/home/codespace/ProyectosCodespace/ProyectosCodespace/build/juegos/CMakeFiles/juegos.dir/src/MenuPrincipal.cpp.o"
+  "/home/codespace/ProyectosCodespace/ProyectosCodespace/juegos/src/Parejas.cpp" "/home/codespace/ProyectosCodespace/ProyectosCodespace/build/juegos/CMakeFiles/juegos.dir/src/Parejas.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../juegos/include"
   )
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/jeffer/Documentos/gitHub/CodespaceClases/juegos/main.cpp" "juegos/CMakeFiles/juegos.dir/main.cpp.o" "gcc" "juegos/CMakeFiles/juegos.dir/main.cpp.o.d"
-  "/home/jeffer/Documentos/gitHub/CodespaceClases/juegos/src/Ahorcado.cpp" "juegos/CMakeFiles/juegos.dir/src/Ahorcado.cpp.o" "gcc" "juegos/CMakeFiles/juegos.dir/src/Ahorcado.cpp.o.d"
-  "/home/jeffer/Documentos/gitHub/CodespaceClases/juegos/src/GestorArchivo.cpp" "juegos/CMakeFiles/juegos.dir/src/GestorArchivo.cpp.o" "gcc" "juegos/CMakeFiles/juegos.dir/src/GestorArchivo.cpp.o.d"
-  "/home/jeffer/Documentos/gitHub/CodespaceClases/juegos/src/Juego.cpp" "juegos/CMakeFiles/juegos.dir/src/Juego.cpp.o" "gcc" "juegos/CMakeFiles/juegos.dir/src/Juego.cpp.o.d"
-  "/home/jeffer/Documentos/gitHub/CodespaceClases/juegos/src/MenuPrincipal.cpp" "juegos/CMakeFiles/juegos.dir/src/MenuPrincipal.cpp.o" "gcc" "juegos/CMakeFiles/juegos.dir/src/MenuPrincipal.cpp.o.d"
-  "/home/jeffer/Documentos/gitHub/CodespaceClases/juegos/src/Parejas.cpp" "juegos/CMakeFiles/juegos.dir/src/Parejas.cpp.o" "gcc" "juegos/CMakeFiles/juegos.dir/src/Parejas.cpp.o.d"
-  "" "bin/juegos" "gcc" "juegos/CMakeFiles/juegos.dir/link.d"
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
