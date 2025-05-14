af63a25c5352c9d329fb3e61832f9f370b498279
Imperiumjervi <rodriguez.jefferson@correounivalle.edu.co>
2025-05-13 20:43:00 -0500

    .

--- taller3/main.cpp
+++ taller3/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include "include/clasepordefecto.hpp"
 
 int main() {
-    ClasePorDefecto obj;
-    obj.saludar();
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
 }
