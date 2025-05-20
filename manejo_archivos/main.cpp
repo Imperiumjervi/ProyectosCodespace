01c76ba1f57457f59ee3753e473a131992d9d4f1
Imperiumjervi <163766795+Imperiumjervi@users.noreply.github.com>
2025-05-20 14:50:31 +0000

    Segmentation fault

--- manejo_archivos/main.cpp
+++ manejo_archivos/main.cpp
@@ -1,18 +1,55 @@
 #include "include/Solution.hpp"
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
+
+vector <Estudiante> separarCadena(string texto);
+
 int main() {
 
+  // fstream archivo("notas.txt", ios::in); <-- Lee el archivo en modo lectura
   ifstream archivo("notas.txt");
   string registro;
+  vector<string> registros;
 
   while(getline(archivo, registro)){
-    cout <<registro<<endl;
+    registros.push_back(registro);
   }
+  separarCadena(registro);
+
+  cout << "tamaño: " << registros.size() << endl;
 
   archivo.close();
   return 0;
+}
+
+vector <Estudiante> separarCadena(string texto) {
+  char delimitador = '-';
+  //string nombre;
+  //float notas[3];
+  //float adicional;
+  //int contador = 0;
+  string auxiliar = "";
+
+  vector<string> contenido;
+
+  for(int i = 0; 0 < texto.size(); i++) {
+    if(i+1 == texto.size()){
+      contenido.push_back(auxiliar);
+      cout << auxiliar << endl;
+      auxiliar = "";
+    } else {
+      if(texto[i] != delimitador){
+        auxiliar += texto[i];
+      }else {
+        contenido.push_back(auxiliar);
+        cout << auxiliar << endl;
+        auxiliar = "";
+      }
+    }
+    
+  }
 }
